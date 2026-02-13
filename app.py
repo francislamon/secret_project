e4371162fe5c90075d56d4a75128fcc03753765f
Francis Lamon <francislamon@gmail.com>
2026-02-13 03:22:22 -0500

    Initial commit: Valentine project

--- app.py
+++ app.py
@@ -7,32 +7,32 @@ def home():
     return render_template('index.html')
 
 @app.route('/yes')
-def yes_page():
+def yes():
     return render_template('yes.html')
 
 @app.route('/yes2')
-def yes_page():
+def yes2():
     return render_template('yes2.html')
 
 @app.route('/yes_yes')
-def yes_page():
+def yes_yes():
 
     return render_template('yes_yes.html')
-@app.route('/yes_yes_yes')
 
-def yes_page():
+@app.route('/yes_yes_yes')
+def yes_yes_yes():
     return render_template('yes_yes_yes.html')
 
 @app.route('/no')
-def no_page():
+def no():
     return render_template('no.html')
 
 @app.route('/no_no')
-def no_page():
+def no_no():
     return render_template('no_no.html')
 
 @app.route('/no_no_no')
-def no_page():
+def no_no_no():
     return render_template('no_no_no.html')
 
 if __name__ == '__main__':
